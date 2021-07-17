16d4ea97854773b877db890209e24d2376e1688a
antrasvez <antreasvezakis@hotmail.com>
2021-07-17 13:07:56 +0300

    MicroServices login Complete

---
+++ MicroServices/Services/Authorization/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "123454",
+  host: "localhost",
+  port: 5432,
+  database: "micro_auth"
+});
+
+module.exports = pool;
