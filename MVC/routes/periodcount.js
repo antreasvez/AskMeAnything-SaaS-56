d7b651e008c0a68f8fffbb221839a9efbedb3182
Efstathios Sarantinopoulos <ssarantin@gmail.com>
2021-07-30 06:07:32 +0300

    more refinements

--- MVC/routes/periodcount.js
+++ MVC/routes/periodcount.js
@@ -6,25 +6,25 @@ require('dotenv').config();
 router.get("/", async (req,res) => {
 
     const day7 = await pool.query(
-        "SELECT count(*) FROM questions WHERE date_created >= CURRENT_DATE - integer '1' ;"
+        "SELECT count(*) FROM questions WHERE qtime >= NOW() - interval '24 hours';"
     )
     const day6 = await pool.query(
-        "SELECT count(*) FROM questions WHERE date_created >= CURRENT_DATE - integer '2' and date_created <= CURRENT_DATE - integer '1';"
+        "SELECT count(*) FROM questions WHERE qtime >= CURRENT_DATE - integer '2' and qtime <= CURRENT_DATE - integer '1';"
     )
     const day5 = await pool.query(
-        "SELECT count(*) FROM questions WHERE date_created >= CURRENT_DATE - integer '3' and date_created <= CURRENT_DATE - integer '2';"
+        "SELECT count(*) FROM questions WHERE qtime >= CURRENT_DATE - integer '3' and qtime <= CURRENT_DATE - integer '2';"
     )
     const day4 = await pool.query(
-        "SELECT count(*) FROM questions WHERE date_created >= CURRENT_DATE - integer '4' and date_created <= CURRENT_DATE - integer '3';"
+        "SELECT count(*) FROM questions WHERE qtime >= CURRENT_DATE - integer '4' and qtime <= CURRENT_DATE - integer '3';"
     )
     const day3 = await pool.query(
-        "SELECT count(*) FROM questions WHERE date_created >= CURRENT_DATE - integer '5' and date_created <= CURRENT_DATE - integer '4';"
+        "SELECT count(*) FROM questions WHERE qtime >= CURRENT_DATE - integer '5' and qtime <= CURRENT_DATE - integer '4';"
     )
     const day2 = await pool.query(
-        "SELECT count(*) FROM questions WHERE date_created >= CURRENT_DATE - integer '6' and date_created <= CURRENT_DATE - integer '5';"
+        "SELECT count(*) FROM questions WHERE qtime >= CURRENT_DATE - integer '6' and qtime <= CURRENT_DATE - integer '5';"
     )
     const day1 = await pool.query(
-        "SELECT count(*) FROM questions WHERE date_created >= CURRENT_DATE - integer '7' and date_created <= CURRENT_DATE - integer '6';"
+        "SELECT count(*) FROM questions WHERE qtime >= CURRENT_DATE - integer '7' and qtime <= CURRENT_DATE - integer '6';"
     )
 
     return res.render("periodcount.ejs", {day1: day1.rows, day2: day2.rows, day3: day3.rows, day4: day4.rows, day5: day5.rows, day6: day6.rows, day7: day7.rows})
