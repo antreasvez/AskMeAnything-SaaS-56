2390fd6dc818be34afdc2d1188127a52eb57407b
Costopoulos <dkns99@gmail.com>
2021-07-31 18:04:40 +0300

    add auth and answer services

--- MicroServices/Services/Answers/routes/postAnswer.js
+++ MicroServices/Services/Answers/routes/postAnswer.js
@@ -2,13 +2,30 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db.js");
 
-router.post("/", async (req, res) => {
+router.get("/:id", async (req,res) => {
+    try{
+      const questionID = req.params.id;
+      const answers = await pool.query(
+          "SELECT answerID, userID, email, questionID, answer_text, to_char(answered_on, 'HH12:MI:SS TZ') FROM answers where questionID=$1;",
+          [questionID]
+      );
+      res.json(answers)
+      
+       } catch (e) {
+        console.log(e.message);
+        res.status(500).send("Server Error");
+       }
+    });
+
+router.post("/:id", async (req, res) => {
   try {
-      const {userID, questionID, answer_text} = req.body;
+      const {userID, email, answer_text} = req.body;
+
+      const questionID = req.params.id;
 
       var answer = await pool.query(
-          "INSERT INTO answers(userID, questionID, answer_text) VALUES ($1, $2, $3) RETURNING *;",
-          [userID, questionID, answer_text]
+          "INSERT INTO answers(userID, email, questionID, answer_text) VALUES ($1, $2, $3, $4) RETURNING *;",
+          [userID, email, questionID, answer_text]
       )
 
       res.json({Message: "Your answer was submitted"})
